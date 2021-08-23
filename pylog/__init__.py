ae65c8ddbc3bf8d82867b4fc6eab9edb3514be9d
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2021-08-23 23:33:07 +0200

    rewrite for better code review

--- pylog/__init__.py
+++ pylog/__init__.py
@@ -1,16 +1,1 @@
-import sys
-
-
-def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('[PYLog] Doing something.')
-
-
-def windows_interaction():
-    assert ('windows' in sys.platform), "Function can only run on Windows systems."
-    print('[PYLog] Doing something on windows.')
-
-
-def mac_interaction():
-    assert ('mac' in sys.platform), "Function can only run on Mac systems."
-    print('[PYLog] Doing something.')
+from ErrorHandle import linux_interaction, windows_interaction, mac_interaction
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+
 classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Education',
+    'Intended Audience :: Developers',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'License :: OSI Approved :: MIT License',
+    'Operating System :: Unix',
+    'Operating System :: MacOS :: MacOS X',
+    'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3'
 ]
 
 setup(
     name='PyLog',
-    version='0.0.1',
+    version=VERSION,
     description='A Python error handler',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='',
