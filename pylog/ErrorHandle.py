da6dbe5ee60d0e51f4f13cf7cb4b215395b9a2b8
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2021-09-14 17:51:22 +0200

    updated error plan

--- pylog/ErrorHandle.py
+++ pylog/ErrorHandle.py
@@ -3,14 +3,36 @@ import sys
 
 def linux_interaction():
     assert ('linux' in sys.platform), "Function can only run on Linux systems."
-    print('[PYLog] Doing something.')
+    logging.basicConfig(filename=loglinux,
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+
+    logging.info("[PYLog]Running log Planning linux")
+
+    print('[PYLog]Doing something.')
 
 
 def windows_interaction():
     assert ('windows' in sys.platform), "Function can only run on Windows systems."
-    print('[PYLog] Doing something on windows.')
+    logging.basicConfig(filename=logwindows,
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+
+    logging.info("[PYLog]Running log Planning windows")
+    print('[PYLog]Doing something on windows.')
 
 
 def mac_interaction():
     assert ('mac' in sys.platform), "Function can only run on Mac systems."
-    print('[PYLog] Doing something.')
+    logging.basicConfig(filename=logmac,
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+
+    logging.info("[PYLog]Running log Planning mac")
+    print('[PYLog]Doing something.')
