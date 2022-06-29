593ca579e01b12aea5889d48e5e47e2dda289292
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2022-06-29 11:00:05 +0200

    added pi to errorhand
    Funstino added, no change to use.

--- pylog/ErrorHandle.py
+++ pylog/ErrorHandle.py
@@ -25,16 +25,21 @@ def error_logger():
 def linux_interaction():
     assert ('linux' in sys.platform), "Function can only run on Linux systems."
 
-    print('[PYLog] Doing something.')
+    print('[PYLog] Doing something. "Linux_28" ')
 
 
 def windows_interaction():
     assert ('windows' in sys.platform), "Function can only run on Windows systems."
 
-    print('[PYLog]Doing something on windows.')
+    print('[PYLog]Doing something on windows. "Windows_34" ')
 
 
 def mac_interaction():
     assert ('mac' in sys.platform), "Function can only run on Mac systems."
 
-    print('[PYLog] Doing something.')
+    print('[PYLog] Doing something. "Mac_40" ')
+
+def mac_interaction():
+    assert ('pi' in sys.platform), "Function can only run on Pi systems."
+
+    print('[PYLog] Doing something. "Pi_45" ')
